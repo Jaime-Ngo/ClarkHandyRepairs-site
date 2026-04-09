3a88d1c771b407315d90aaab9b341441d67cd021
riverkngo <riverkngo@gmail.com>
2026-04-08 19:20:23 -0700

    fixed sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://clarkhandysolutions.com',
+  site: 'https://www.clarkhandysolutions.com',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
